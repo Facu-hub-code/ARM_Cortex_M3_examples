7e5b1ffc57851be85baabe4f2f8a523f56a19ac0
Facundo Lorenzo <68453608+Facu-hub-code@users.noreply.github.com>
2022-09-29 07:48:05 -0300

    timer en modo capture

--- timer_ayarde.c
+++ timer_ayarde.c
@@ -7,6 +7,8 @@ Y la salida esetaba en cero que la lleve a uno, a su vez que resetee el timer pa
 
 void confTimer(void);
 
+unit32_t aux = 0;
+
 int main(void){
 	confGPIO();
 	confTimer();
@@ -27,11 +29,12 @@ Configuro el timmer 0:
 - EMR determina la funcionalidad del match 0: puede togglear la salida del pin correspondiente
 - MCR cada vez que se genere la condicion de match, va generar una interrupcion.
 - TCR 
-*/
+
+//Configuracion en modo match
 void confTimer(){
 	LPC_SC->PCONP		|=	(1<<1); //pag 65 
 	LPC_SC->PCLKSEL0 	|=	(1<<2); //pag 59
-	LPC_PINCON->PINSEL3	|=	(1<<1); //pag 120
+	LPC_PINCON->PINSEL3	|=	(3<<20); //pag 120
 	LPC_TIM0->EMR		|=	(3>>4); //pag 509
 	LPC_TIM0->MR0 		= 	70000000;
 	LPC_TIM0->MCR 		|= 	(1<<1);	//pag 507
@@ -39,10 +42,31 @@ void confTimer(){
 	LPC_TIM0->TCR		=	3		//pag 505
 	LPC_TIM0->TCR		&=	~(1<<1)
 	NVIC_EnableIRQ(TIMER0_IRQn);
+	return;
+}
+*/
+
+/*Configuracion en modo captura
+	- Vamos a esperar una entrada en el pin 20
+	- El evento que vaya a disparar sera con un flanco de bajada
+	- Y genera una interrupcion
+*/
+void confTimer(){
+	LPC_SC->PCONP		|=	(1<<1); //pag 65 
+	LPC_SC->PCLKSEL0 	|=	(1<<2); //pag 59
+	LPC_PINCON->PINSEL3	|=	(3<<20); //pag 120
+	LPC_TIM0->CCR		|=	(1<<1)|(1<<2);//pag 508
+	LPC_TIM0->TCR   	|=	3; //pag 505
+	LPC_TIM0->TCR 		&=	~(1<<1);
+	NVIC_EnableIRQ(TIMER0_IRQn);
+	return;
 }
 
+//Si se genero esa interrupcion de flanco de bajada
+//usamos la misma interrupcion
 void TIMER0_IRQnHandler(void){
-	static unit8_t i == 0;
+	static	unit8_t i = 0;   
+	aux = LPC_TIM0->CR0; //Guardo lo que capturo el modulo timmer.
 	if (i==0)
 	{
 		LPC_GPIO0->FIOSET	=	(1<<22);
@@ -51,7 +75,9 @@ void TIMER0_IRQnHandler(void){
 		LPC_TIM0->IR |=	1;
 		i = 0;
 	}
-	LPC_TIM0->IR |= 1;
+	LPC_TIM0->IR |= 1; //Limpiamos la bandera de interrupcion.
 	return;
 }
-//Video 2 minuto 35 explica la configuracion del timmer de forma resumida
+//Video 2 minuto 35: Explica la configuracion del timmer de forma resumida
+//Video 3 minuto 50: Muestra como mostrar una variable global
+//Video 3 minuto   : 
