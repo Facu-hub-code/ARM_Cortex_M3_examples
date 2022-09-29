67de1de7b22d652b1463c467b90e4ad98708c8c5
Facundo Lorenzo <68453608+Facu-hub-code@users.noreply.github.com>
2022-09-28 23:48:57 -0300

    handler timer0 modo match

--- timer_ayarde.c
+++ timer_ayarde.c
@@ -8,10 +8,15 @@ Y la salida esetaba en cero que la lleve a uno, a su vez que resetee el timer pa
 void confTimer(void);
 
 int main(void){
-	//confGPIO();
+	confGPIO();
 	confTimer();
 	while(1){}
 	return 0;
+}
+
+void confGPIO(void){
+	LPC_GPIO0->FIODIR	|=	(1<<22);
+	return;
 }
 
 /*
@@ -29,8 +34,24 @@ void confTimer(){
 	LPC_PINCON->PINSEL3	|=	(1<<1); //pag 120
 	LPC_TIM0->EMR		|=	(3>>4); //pag 509
 	LPC_TIM0->MR0 		= 	70000000;
-	LPC_TIM0->MCR 		= 	2		//pag 507
+	LPC_TIM0->MCR 		|= 	(1<<1);	//pag 507
+	LPC_TIM0->MCR 		|= 	(1<<0);	  
 	LPC_TIM0->TCR		=	3		//pag 505
 	LPC_TIM0->TCR		&=	~(1<<1)
-	// NVIC_EnableIRQ(TIMER0_IRQn);
+	NVIC_EnableIRQ(TIMER0_IRQn);
 }
+
+void TIMER0_IRQnHandler(void){
+	static unit8_t i == 0;
+	if (i==0)
+	{
+		LPC_GPIO0->FIOSET	=	(1<<22);
+		i = 1;
+	}else if(i == 1){
+		LPC_TIM0->IR |=	1;
+		i = 0;
+	}
+	LPC_TIM0->IR |= 1;
+	return;
+}
+//Video 2 minuto 35 explica la configuracion del timmer de forma resumida
